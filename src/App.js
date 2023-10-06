966273675f57d7866b3c632c8d3546c7bdb7c763
Seerat Shaikh <seeratshaikh579@gmail.com>
2023-10-06 19:58:56 +0500

    Dwarf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Planets from './components/Planets';
+import Dwarfplanets from './components/Dwarfplanets';
 import Moon from './components/Moon';
+
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/planets" element={<Planets />} />
+        <Route path="/dwarf" element={<Dwarfplanets />} />
+
         <Route path="/moon" element={<Moon />} />
 
       </Routes>
